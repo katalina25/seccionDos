1dc0b6920b2fdc20385111239b7c8c4d50074c0a
Katalina25 <katalina.guerrero02@inacapmail.cl>
2021-03-03 21:47:22 -0300

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './account/login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: 'app', component: AppComponent },
+      { path: 'login', component: LoginComponent },
+      { path: '', redirectTo: '/login', pathMatch: 'full' },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
